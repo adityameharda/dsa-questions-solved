19c1301ba952876449550497cec27656ee8235fb
Aditya Meharda <adityamehrda2005@gmail.com>
2026-03-08 16:29:14 +0530

    Time: 0 ms (100%), Space: 12.9 MB (49.05%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -10,20 +10,22 @@ public:
         }
         sort(v.begin(), v.end());
         int y = 0;
-        for(int i = 0; i < (1 << n); i++)
+        int c = 2*n;
+        for(int i = 0 ; i < c ; i++) 
         {
-            if(i >= v.size() || v[i] != i)
+            if(i >= v.size() || i != v[i])
             {
-                y = i;
+                y = i ;
                 break;
             }
         }
-        string ans = "";
-        for(int i = n - 1; i >= 0; i--)
-        {
-            if(y & (1 << i)) ans += '1';
-            else ans += '0';
-        }
+        // string ans = "";
+        // for(int i = n - 1; i >= 0; i--)
+        // {
+        //     if(y & (1 << i)) ans += '1';
+        //     else ans += '0';
+        // }
+        string ans = bitset<16>(y).to_string().substr(16-n);
 
         return ans;
     }
