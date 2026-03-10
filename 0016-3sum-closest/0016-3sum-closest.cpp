5faa7c0e347c828efefdcf051e8ebf47ba18f84f
Aditya Meharda <adityamehrda2005@gmail.com>
2026-03-10 22:50:21 +0530

    Time: 4 ms (94.75%), Space: 14.1 MB (32.44%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,30 +1,78 @@
+// class Solution {
+// public:
+//     int threeSumClosest(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         // sort(nums.begin(),nums.end());
+//         // for(int i = 0 ; i < n ; i++)
+//         // {
+//         //     for(int j = i ; j < n ;j++)
+//         //         if(nums[i] == nums[j]) continue;
+//         // }
+//         int res =INT_MAX ,ans = 0,t,p;
+//         for(int i = 0 ; i < n ; i++)
+//         {
+//             for(int j = i+1 ; j < n ; j++)
+//             {
+//                 for(int k = j+1 ; k < n ; k++)
+//                 {
+//                     ans = nums[i]+nums[j]+nums[k];
+//                     t = abs(ans-target);
+//                     if(t < res)
+//                     {
+//                         res = t;
+//                         p = ans;
+//                     }
+//                 }
+//             }
+//         }
+//             return p;
+//     }
+// };
+
+
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
         int n = nums.size();
-        // sort(nums.begin(),nums.end());
-        // for(int i = 0 ; i < n ; i++)
-        // {
-        //     for(int j = i ; j < n ;j++)
-        //         if(nums[i] == nums[j]) continue;
-        // }
-        int res =INT_MAX ,ans = 0,t,p;
-        for(int i = 0 ; i < n ; i++)
+        sort(nums.begin(), nums.end());
+
+        int closest = nums[0] + nums[1] + nums[2];
+
+        for(int i = 0; i < n - 2; i++)
         {
-            for(int j = i+1 ; j < n ; j++)
+            int j = i + 1;
+            int k = n - 1;
+
+            while(j < k)
             {
-                for(int k = j+1 ; k < n ; k++)
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if(abs(sum - target) < abs(closest - target))
                 {
-                    ans = nums[i]+nums[j]+nums[k];
-                    t = abs(ans-target);
-                    if(t < res)
-                    {
-                        res = t;
-                        p = ans;
-                    }
+                    closest = sum;
+                }
+
+                if(sum < target)
+                {
+                    j++;
+                }
+                else if(sum > target)
+                {
+                    k--;
+                }
+                else
+                {
+                    return sum;
                 }
             }
         }
-            return p;
+
+        return closest;
     }
 };
+
+
+
+
+
+
