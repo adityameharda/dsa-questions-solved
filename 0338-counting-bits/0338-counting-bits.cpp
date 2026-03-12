d989e8157eaf284912129f05c3faf6669c7bdb6c
Aditya Meharda <adityamehrda2005@gmail.com>
2026-03-12 20:18:39 +0530

    Time: 0 ms (100%), Space: 10.4 MB (97.14%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>v(n+1);
+        if(n==0){
+              v[0] = 0;
+              return v;
+        }
+      
+        if(n==1){
+            v[0] = 0;
+            v[1] = 1;
+            return v;
+        }
+        v[0] = 0;
+        v[1] = 1;
+        for(int i = 2 ; i <= n ; i++)
+        {
+            if(i%2 == 0)
+            {
+                v[i] = v[i/2] + 0 ;
+            }else v[i] = v[i/2] + 1 ;
+        }
+        return v;
+    }
+};
