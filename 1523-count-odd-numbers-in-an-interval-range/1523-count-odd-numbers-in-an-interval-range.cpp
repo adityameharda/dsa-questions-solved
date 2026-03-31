527feaa73b41d80d33a67bac9d6659bf237fc170
Aditya Meharda <adityamehrda2005@gmail.com>
2026-03-31 23:51:07 +0530

    Time: 3 ms (60.09%), Space: 7.9 MB (39.48%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -1,17 +1,21 @@
+// class Solution {
+// public:
+//     int countOdds(int low, int high) {
+//         int count = 0;
+//         while(low<=high)
+//         {
+//             if(low %2 != 0)count++;
+
+//             low++;
+//         }
+//         return count;
+//     }
+// };
+
+
 class Solution {
 public:
     int countOdds(int low, int high) {
-        int count = 0;
-        // for(int i = low ; i < high ; i++)
-        // {
-        //     if()
-        // }
-        while(low<=high)
-        {
-            if(low %2 != 0)count++;
-
-            low++;
-        }
-        return count;
+        return (high + 1)/2 - (low)/2;
     }
 };
