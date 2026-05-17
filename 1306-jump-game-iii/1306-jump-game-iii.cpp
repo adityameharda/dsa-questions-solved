559e86fdd6578fbe04c60a797fde8c75bfe57090
Aditya Meharda <adityamehrda2005@gmail.com>
2026-05-17 14:25:18 +0530

    Time: 3 ms (45.57%), Space: 38.6 MB (47.62%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,30 +1,48 @@
+// class Solution {
+// public:
+//     bool canReach(vector<int>& arr, int start) {
+//         int n = arr.size();
+//         vector<int>vis(n,0);
+//         queue<int>q;
+//         q.push(start);
+//         while(!q.empty())
+//         {
+//             int curr = q.front();
+//             q.pop();
+//             if(curr < 0 || curr >= n || vis[curr] == 1)
+//             {
+//                 continue;
+//             }
+//             if(arr[curr] == 0)
+//             {
+//                 return true;
+//             }
+//             vis[curr] = 1;
+//             int t1 = curr - arr[curr];
+//             int t2 = curr + arr[curr];
+//             q.push(t1);
+//             q.push(t2);
+//         }
+
+//         return false;
+
+//     }
+// };
 class Solution {
 public:
     bool canReach(vector<int>& arr, int start) {
         int n = arr.size();
-        vector<int>vis(n,0);
-        queue<int>q;
-        q.push(start);
-        while(!q.empty())
+        if(start < 0 || start >= n || arr[start] < 0)
         {
-            int curr = q.front();
-            q.pop();
-            if(curr < 0 || curr >= n || vis[curr] == 1)
-            {
-                continue;
-            }
-            if(arr[curr] == 0)
-            {
-                return true;
-            }
-            vis[curr] = 1;
-            int t1 = curr - arr[curr];
-            int t2 = curr + arr[curr];
-            q.push(t1);
-            q.push(t2);
+            return false;
         }
-
-        return false;
-
+        if(arr[start] == 0)
+        {
+            return true;
+        }
+        int jump = arr[start];
+        arr[start] = -1;
+        return canReach(arr, start + jump) || 
+               canReach(arr, start - jump);
     }
 };
