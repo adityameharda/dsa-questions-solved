2ee64d68a1b3792b12443011c0e471d2a00cc6ce
Aditya Meharda <adityamehrda2005@gmail.com>
2026-09-08 19:54:16 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.38%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int ans = 0;
+        for (int x = 1000 ; x <= n ; x *= 1000) {
+            ans +=n-x+1;
+        }
+        
+        return ans;
+    }
+};
